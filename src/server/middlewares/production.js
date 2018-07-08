9e0bf7b5a69ad082dd029f7c419cd0ba62ea2e7d
wenfu <wenfu.zwf@alibaba-inc.com>
2018-07-08 15:16:13 +0800

    fix: add script

--- src/server/middlewares/production.js
+++ src/server/middlewares/production.js
@@ -7,7 +7,4 @@ const clientBuildPath = resolve(__dirname, '..', '..', 'client');
 module.exports = function setup(app) {
   app.use(compression());
   app.use('/', express.static(clientBuildPath));
-
-  // all other requests be handled by UI itself
-  app.get('*', (req, res) => res.sendFile(resolve(clientBuildPath, 'index.html')));
 };
